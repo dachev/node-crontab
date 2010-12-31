0982f0a58eb017dfee961d5e3650259b2531a469
Blagovest Dachev <blago@dachev.com>
2010-12-30 22:58:23 -0600

    Created io tests

--- test/runner.js
+++ test/runner.js
@@ -2,13 +2,63 @@
 
 var Vows    = require('vows'),
     Assert  = require('assert'),
-    Crontab = require('../lib/crontab');
+    CronTab = require('../lib/crontab');
 
 Vows.describe('crontab').addBatch({
-    'can load user crontab': {
-        topic:3,
-        'test':function(v) {
-            Assert.equal(v, 3);
+    'io': {
+        topic: loadTabs,
+        'will load user tabs':function(err, tab) {
+            Assert.isNull(err);
+            Assert.isObject(tab);
+        },
+        'after tabs are loaded': {
+            topic:saveTabs,
+            'will save user tabs':function(err, tab) {
+                Assert.isNull(err);
+                Assert.isObject(tab);
+            },
+            'then saved': {
+                topic: loadTabs,
+                'will load user tabs again':function(err, tab) {
+                    Assert.isNull(err);
+                    Assert.isObject(tab);
+                },
+                'and then loaded again': {
+                    topic: makeTabArrayFromTopicStack,
+                    'they will be the same':function(tabs) {
+                        Assert.equal(tabs[0].render(), tabs[1].render());
+                    }
+                }
+            }
         }
     }
 }).export(module);
+
+function loadTabs() {
+    var promise = new(process.EventEmitter);
+    
+    CronTab.load('', function(err, tab) {
+        if (err) { promise.emit('error', err); }
+        else     { promise.emit('success', tab); }
+    });
+    
+    return promise;
+}
+
+function saveTabs(tab) {
+    var promise = new(process.EventEmitter);
+    
+    tab.save(function(err, tab) {
+        if (err) { promise.emit('error', err); }
+        else     { promise.emit('success', tab); }
+    });
+    
+    return promise;
+}
+
+function makeTabArrayFromTopicStack() {
+    var tab1 = arguments[0],
+        tab2 = arguments[2];
+    
+    return [tab1, tab2];
+}
